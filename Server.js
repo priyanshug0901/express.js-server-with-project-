e2d6ba2c8ebaef2e6f2dab484cbdd2ee234a8a2b
priyanshug0901 <priyanshug0901@gmail.com>
2021-03-22 03:48:55 +0530

    added : static route

--- Server.js
+++ Server.js
@@ -1,14 +1,17 @@
 const express = require ('express');
 const app = express(); //contain all methods
 const path=require('path');
-
-
 const PORT=process.env.PORT || 3000;
 
-app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname)+'/index.html');
-});
+app.use(express.static('public'));
 
+
+// app.get('/',(req,res) => {
+//     res.sendFile(path.resolve(__dirname)+'/index.html');
+// });
+// app.get('/about',(req,res) => {
+//     res.sendFile(path.resolve(__dirname)+'/about.html');
+// });
 app.listen(PORT,() => console.log(`listening on port ${PORT}`)
 ); //to create server
 
