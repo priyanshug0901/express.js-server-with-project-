169bbb6828c079ed24dd02e7aa1e41c662833084
priyanshug0901 <priyanshug0901@gmail.com>
2021-03-22 15:57:22 +0530

    added: ejs engine, changed: extension of index and about page to .ejs, used: template engine to render index,about file as views from vieews folder.

--- Server.js
+++ Server.js
@@ -1,17 +1,36 @@
 const express = require ('express');
-const app = express(); //contain all methods
 const path=require('path');
+const app = express(); //contain all methods
 const PORT=process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.set('view engine','ejs'); //fpr setting template egine
+
+// app.set('views',path.resolve(__dirname)+'/templates'); // to change views directory
+// console.log(app.get('view engine'));
+// console.log(app.get('views')); //to print views folder path 
 
 
-// app.get('/',(req,res) => {
-//     res.sendFile(path.resolve(__dirname)+'/index.html');
-// });
-// app.get('/about',(req,res) => {
-//     res.sendFile(path.resolve(__dirname)+'/about.html');
-// });
+
+
+
+//app.use(express.static('public')); //used for static route{middle ware}
+
+app.get('/',(req,res) => {
+   // res.sendFile(path.resolve(__dirname)+'/index.html');
+    res.render('index',{title:'home page'});  //rendering file using templating engine
+});
+
+app.get('/about',(req,res) => {
+    // res.sendFile(path.resolve(__dirname)+'/about.html');
+    res.render('about',{title:'my about page'}); 
+});
+
+app.get('/download',(req,res) => {                         //serving file for download 
+    res.download(path.resolve(__dirname)+'/about.html ')
+})
+
+
+
 app.listen(PORT,() => console.log(`listening on port ${PORT}`)
 ); //to create server
 
